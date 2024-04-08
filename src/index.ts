71fd574c8d00eb92110922b9d2a4c7b6f3df2cc0
lihaipeng <lihaipeng@shanlitech.com>
2024-04-08 16:09:36 +0800

    fix: 修复版本问题

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,14 @@ import { getFileList } from "./utils/index.js";
 import { version } from "./const/index.js";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import fs from "fs-extra";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const nodeV = process.versions;
-const start = async () => {
-  program.version(version); // 设置版本
+const start = async (packageData) => {
+  console.log("当前版本", packageData.version);
+  program.version(packageData.version); // 设置版本
   const fileList = await getFileList(join(__dirname, "commands"));
   const filePromise = [];
   fileList.forEach((fileName) => {
@@ -36,7 +38,11 @@ try {
   if (Number(nodeVNUmber) < 18120) {
     console.log("node版本需>=18.12.0");
   } else {
-    start();
+    fs.readFile("./package.json", "utf-8").then((data) => {
+      const packageData = JSON.parse(data);
+      start(packageData);
+    });
+    // start();
   }
 } catch (err) {
   console.error("异常", err);
